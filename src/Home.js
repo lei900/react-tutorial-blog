b61f482ebbe65be1abc2b1c626fc5ce7eabb5fab
lei <mia.wang90@gmail.com>
2022-10-28 18:50:39 +0900

    Fetching data with useEffect

---
+++ src/Home.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import BlogList from "./BlogList";
+
+const Home = () => {
+  const [blogs, setBlogs] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/blogs")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setBlogs(data);
+      });
+  }, []);
+
+  return (
+    <div className="home">
+      {blogs && <BlogList blogs={blogs} title="All Blogs" />}
+    </div>
+  );
+};
+
+export default Home;
