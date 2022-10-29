c4f99f08c26e32fea529c7fe69cefa8535d50194
lei <mia.wang90@gmail.com>
2022-10-29 14:19:13 +0900

    Add route parameters

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from "./Navbar";
 import Home from "./Home";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Create from "./Create";
+import BlogDetails from "./BlogDetails";
 
 function App() {
   return (
@@ -11,9 +12,8 @@ function App() {
         <div className="content">
           <Routes>
             <Route path="/" element={<Home />} />
-          </Routes>
-          <Routes>
             <Route path="/create" element={<Create />} />
+            <Route path="/blogs/:id" element={<BlogDetails />} />
           </Routes>
         </div>
       </div>
